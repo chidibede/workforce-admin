a174bcda0c7bfac136f1dbda4ff6e39b8d4c28bf
Chidi Enwereji <chidibede@gmail.com>
2024-09-10 20:23:41 +0100

    save changes

--- src/services/search.js
+++ src/services/search.js
@@ -2,8 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import supabase from "./supabase";
 
 const searchWorkers = async (searchParams) => {
-  if(!searchParams) return []
-  const { data, error } = await supabase.rpc('get_search_results', { search_text: searchParams.trim()} );
+  if (!searchParams) return [];
+  const { data, error } = await supabase.rpc("get_search_results", {
+    search_text: searchParams.trim(),
+  });
   if (error) {
     throw new Error(error.message);
   }
